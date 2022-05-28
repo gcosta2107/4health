88b45d2535aa0ddf7b9ebab4af9f7386fcec3330
gcosta2107 <garragr10@gmail.com>
2022-05-28 02:12:31 -0300

    add envio whatsapp

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from leitura_arquivos import leitura_frases
+from automacao_whatsapp import enviar_pergunta
 
 name = input("Nome do contato: ")
 num_file = int(input("Informe a lista: "))
-msg = input("Informe o numero da pergunta: ")
+msg = int(input("Informe o numero da pergunta: "))
 
 try:
     if (num_file == 1):
         frases = leitura_frases("arquivos/listapergunta1.txt")
-        print(frases)
+        pergunta_enviada = frases[msg - 1]
+        enviar_pergunta(name,pergunta_enviada)
         
     elif (num_file == 2):
         frases = leitura_frases("arquivos/listapergunta1.txt")
+        pergunta_enviada = frases[msg - 1]
+        enviar_pergunta(name,pergunta_enviada)
 
     elif (num_file == 3):
         frases = leitura_frases("arquivos/listapergunta1.txt")
+        pergunta_enviada = frases[msg - 1]
+        enviar_pergunta(name,pergunta_enviada)
 
     elif (num_file == 4):
         frases = leitura_frases("arquivos/listapergunta1.txt")
+        pergunta_enviada = frases[msg - 1]
+        enviar_pergunta(name,pergunta_enviada)
 
 except FileNotFoundError:
     print("Lista inexistente")
